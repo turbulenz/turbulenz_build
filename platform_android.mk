09d1174908160c34dcf631cd0bbf6ff12e531232
Duncan Tebbs <duncantebbs@turbulenz.com>
2012-08-07 19:57:03 +0900

    Support for android-ndk-r8b.  Tidy up of android build variables

--- platform_android.mk
+++ platform_android.mk
@@ -5,54 +5,55 @@
 
 # NDK dir
 
-ANDROID_NDK ?= external/android/android-ndk-r8
-ANDROID_NDK_PLATFORM ?= android-9
+ANDROID_NDK ?= external/android/android-ndk-r8b
+NDK_PLATFORM ?= android-9
+NDK_GCC_VER ?= 4.6
 
 # Toolset for which arch
 
 ifeq ($(ARCH),armv7a)
-  ANDROID_NDK_ARCHDIR := $(ANDROID_NDK)/toolchains/arm-linux-androideabi-4.4.3
-  ANDROID_NDK_TOOLPREFIX := arm-linux-androideabi-
-  ANDROID_NDK_PLATFORMDIR := \
-    $(ANDROID_NDK)/platforms/$(ANDROID_NDK_PLATFORM)/arch-arm
-  ANDROID_NDK_STL_LIBS += \
-    $(ANDROID_NDK)/sources/cxx-stl/gnu-libstdc++/libs/armeabi-v7a
+  NDK_ARCHDIR := $(ANDROID_NDK)/toolchains/arm-linux-androideabi-$(NDK_GCC_VER)
+  NDK_TOOLPREFIX := arm-linux-androideabi-
+  NDK_PLATFORMDIR := \
+    $(ANDROID_NDK)/platforms/$(NDK_PLATFORM)/arch-arm
   ANDROID_ARCH_NAME := armeabi-v7a
 endif
 ifeq ($(ARCH),x86)
-  ANDROID_NDK_ARCHDIR := $(ANDROID_NDK)/toolchains/x86-4.4.3
-  ANDROID_NDK_TOOLPREFIX := i686-android-linux-
-  ANDROID_NDK_PLATFORMDIR := \
-    $(ANDROID_NDK)/platforms/$(ANDROID_NDK_PLATFORM)/arch-x86
-  ANDROID_NDK_STL_LIBS += \
-    $(ANDROID_NDK)/sources/cxx-stl/gnu-libstdc++/libs/x86
+  NDK_ARCHDIR := $(ANDROID_NDK)/toolchains/x86-$(NDK_GCC_VER)
+  NDK_TOOLPREFIX := i686-linux-android-
+  NDK_PLATFORMDIR := \
+    $(ANDROID_NDK)/platforms/$(NDK_PLATFORM)/arch-x86
   ANDROID_ARCH_NAME := x86
 endif
-ifeq ($(ANDROID_NDK_ARCHDIR),)
+ifeq ($(NDK_ARCHDIR),)
   $(error Couldnt determine toolchain for android ARCH $(ARCH))
 endif
 
 # Find toolset for this platfom
 
 ifeq ($(BUILDHOST),macosx)
-  ANDROID_NDK_TOOLBIN := $(ANDROID_NDK_ARCHDIR)/prebuilt/darwin-x86/bin
+  NDK_TOOLBIN := $(NDK_ARCHDIR)/prebuilt/darwin-x86/bin
 endif
 ifeq ($(BUILDHOST),linux64)
-  ANDROID_NDK_TOOLBIN := $(error set this) \
-	$(ANDROID_NDK_ARCHDIR)/prebuilt/linux-x86/bin
+  NDK_TOOLBIN := $(error set this) \
+	$(NDK_ARCHDIR)/prebuilt/linux-x86/bin
 endif
-ifeq ($(ANDROID_NDK_TOOLBIN),)
+ifeq ($(NDK_TOOLBIN),)
   $(error Couldnt find toolchain for BUILDHOST $(BUILDHOST))
 endif
 
 # Some include paths
 
-ANDROID_NDK_STL_INCLUDES := \
-  $(ANDROID_NDK_STL_LIBS)/include \
-  $(ANDROID_NDK)/sources/cxx-stl/gnu-libstdc++/include
-ANDROID_NDK_PLATFORM_INCLUDES := \
+NDK_STL_DIR := \
+  $(ANDROID_NDK)/sources/cxx-stl/gnu-libstdc++/$(NDK_GCC_VER)
+NDK_STL_LIBS += \
+  $(NDK_STL_DIR)/libs/$(ANDROID_ARCH_NAME)
+
+NDK_STL_INCLUDES := \
+  $(NDK_STL_LIBS)/include $(NDK_STL_DIR)/include
+NDK_PLATFORM_INCLUDES := \
   $(ANDROID_NDK)/sources/android/native_app_glue \
-  $(ANDROID_NDK_PLATFORMDIR)/usr/include
+  $(NDK_PLATFORMDIR)/usr/include
 
 # Set the variant to incldue the arch
 
@@ -116,7 +117,7 @@ VARIANT:=$(strip $(VARIANT)-$(ARCH))
 
 
 
-CXX := $(ANDROID_NDK_TOOLBIN)/$(ANDROID_NDK_TOOLPREFIX)g++
+CXX := $(NDK_TOOLBIN)/$(NDK_TOOLPREFIX)g++
 CXXFLAGSPRE := \
   -ffunction-sections -funwind-tables -fno-rtti \
   -fomit-frame-pointer -fstrict-aliasing -funswitch-loops \
@@ -139,7 +140,7 @@ ifeq ($(ARCH),x86)
 endif
 
 CXXFLAGSPOST := \
- $(addprefix -I,$(ANDROID_NDK_STL_INCLUDES) $(ANDROID_NDK_PLATFORM_INCLUDES)) \
+ $(addprefix -I,$(NDK_STL_INCLUDES) $(NDK_PLATFORM_INCLUDES)) \
  -DFASTCALL= -finline-limit=256 -O3 -Wa,--noexecstack -O2 -fexceptions
 
 ifeq ($(CONFIG),debug)
@@ -156,7 +157,7 @@ CXXFLAGSPOST += -c
 # AR
 #
 
-AR := $(ANDROID_NDK_TOOLBIN)/$(ANDROID_NDK_TOOLPREFIX)ar
+AR := $(NDK_TOOLBIN)/$(NDK_TOOLPREFIX)ar
 ARFLAGSPRE := cr
 arout :=
 ARFLAGSPOST :=
@@ -180,16 +181,16 @@ libsuffix := .a
 # -Wl,--fix-cortex-a8  -Wl,--no-undefined -Wl,-z,noexecstack -L/Users/dtebbs/turbulenz/external/android/android-ndk-r8/platforms/android-9/arch-arm/usr/lib -llog -landroid -lEGL -lGLESv2 -ldl -llog -lc -lm -o /Users/dtebbs/turbulenz/build/android/obj/local/armeabi-v7a/libturbulenz.so
 
 
-DLL := $(ANDROID_NDK_TOOLBIN)/$(ANDROID_NDK_TOOLPREFIX)gcc
+DLL := $(NDK_TOOLBIN)/$(NDK_TOOLPREFIX)gcc
 DLLFLAGSPRE := -Wl,-soname,$$(basename $$@) -shared \
-  --sysroot=$(ANDROID_NDK_PLATFORMDIR) \
+  --sysroot=$(NDK_PLATFORMDIR) \
 # -nostdlib
 # -Wl,-shared,-Bsymbolic
 
 DLLFLAGSPOST := \
-  $(ANDROID_NDK_STL_LIBS)/libgnustl_static.a \
+  $(NDK_STL_LIBS)/libgnustl_static.a \
   -Wl,--no-undefined -Wl,-z,noexecstack \
-  -L$(ANDROID_NDK_PLATFORMDIR)/usr/lib \
+  -L$(NDK_PLATFORMDIR)/usr/lib \
   -landroid -lEGL -lGLESv2 -ldl -llog -lc -lm
 
 ifeq ($(ARCH),armv7a)
