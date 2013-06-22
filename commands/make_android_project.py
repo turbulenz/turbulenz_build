a742b7e061022ec27974d773645b7bbf9c09f7eb
Duncan Tebbs <duncantebbs@turbulenz.com>
2013-06-22 23:54:30 +0900

    Send screenSize config changes to android activity, rather than letting the OS handle them

--- commands/make_android_project.py
+++ commands/make_android_project.py
@@ -343,7 +343,7 @@ def write_manifest(dest, table, permissions, intent_filters, meta,
     if options['landscape']:
         MANIFEST_0 += """
                   android:screenOrientation="landscape"
-                  android:configChanges="orientation" """
+                  android:configChanges="orientation|screenSize" """
 
     MANIFEST_0 += """
                   >"""
