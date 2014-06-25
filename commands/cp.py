a37affe781d7cc7d2c8a4b688efd831ace42c12f
Duncan Tebbs <duncantebbs@turbulenz.com>
2014-06-25 09:51:58 +0000

    Wildcards in cp.py

--- commands/cp.py
+++ commands/cp.py
@@ -2,14 +2,17 @@
 
 import shutil
 import sys
+import glob
 
 all_files = sys.argv[1:]
 if len(all_files) < 2:
     usage()
     exit(1)
 
-src_files = all_files[:-1]
+src_args = all_files[:-1]
 dest = all_files[-1]
 
-for s in src_files:
-    shutil.copy(s, dest)
+for src_pattern in src_args:
+    src_files = glob.glob(src_pattern)
+    for src in src_files:
+        shutil.copy(src, dest)
