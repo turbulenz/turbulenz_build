e7f707ee4d8729928706c26ab7430d9ff369cb1e
Duncan Tebbs <duncantebbs@turbulenz.com>
2014-10-30 05:36:11 +0000

    Better valgrind flags.  Pass TZ_VALGRIND for valgrind builds.

--- config.mk
+++ config.mk
@@ -163,8 +163,9 @@ ifneq ($(CMDVERBOSE),1)
 endif
 
 ifeq ($(VALGRIND),1)
-  RUNPREFIX+=valgrind --dsymutil=yes --leak-check=full --error-exitcode=15
+  RUNPREFIX+=valgrind --dsymutil=yes --track-origins=yes --leak-check=full --error-exitcode=15
   # --gen-suppressions=all
+  CXXFLAGS += -DTZ_VALGRIND
 endif
 
 CP := python $(BUILDDIR)/commands/cp.py
