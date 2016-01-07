80a76d82f871baac8faa01bb1e1d16ce25af2985
Duncan Tebbs <duncan.tebbs@gmail.com>
2016-01-07 17:25:03 +0900

    split config steps into bsic info about the platform, and the full location of compilers/ flags, etc

--- config.mk
+++ config.mk
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Turbulenz Limited.
+# Copyright (c) 2015 Turbulenz Limited.
 # Released under "Modified BSD License".  See COPYING for full text.
 
 ifeq ($(BUILDDIR),)
@@ -16,152 +16,12 @@ ABSPATHS ?= 1
 # Disable all build-in rules
 .SUFFIXES:
 
-#
-# Platform stuff.  Determine the build host
-#
+############################################################
+# Get basic platform info (HOST and TARGET)
+############################################################
 
-# Try to detect Windows, most robust methods first
-
-ifeq (Windows_NT,$(OS))
-  UNAME := win32
-else
-  ifneq (,$(filter %.exe,$(notdir $(SHELL))))
-    UNAME := win32
-  else
-    ifdef SYSTEMROOT
-      UNAME := win32
-    else
-      ifdef SystemRoot
-        UNAME := win32
-      else
-        ifdef COMSPEC
-          UNAME := win32
-        else
-          ifdef ComSpec
-            UNAME := win32
-          endif
-        endif
-      endif
-    endif
-  endif
-endif
-
-ifeq (,$(UNAME))
-  UNAME := $(shell uname)
-endif
-
-# Check for unsupported build host
-ifeq ($(UNAME),)
-  $(warning Couldnt determine BUILDHOST from uname: $(UNAME), assuming win32)
-  UNAME := win32
-endif
-
-# macosx
-ifeq ($(UNAME),Darwin)
-  BUILDHOST := macosx
-endif
-
-# linux32/64
-ifeq ($(UNAME),Linux)
-  M_ARCH := $(shell uname -m)
-  ifeq ($(M_ARCH),x86_64)
-    BUILDHOST := linux64
-  else
-    ifeq ($(M_ARCH),i686)
-      BUILDHOST := linux32
-    else
-      $(error Unsupported architecture: $(M_ARCH))
-    endif
-  endif
-endif
-
-# windows
-ifeq ($(UNAME),win32)
-  override SHELL := cmd.exe
-  BUILDHOST := win64
-  ABSPATHS := 0
-  UNITY := 0
-endif
-
-# $(info UNAME = $(UNAME))
-# $(info SHELL = $(SHELL))
-
-# Set TARGET if it hasn't been determined, and based on that, set
-# TARGETNAME:
-#
-#  TARGET = linux32, linux64, macosx, win32, win64, android, ...
-#  TARGETNAME = linux, macosx, win, android, ...
-#  ARCH = i386,x86_64,armv7a
-#  PKGARCH = x86,amd64
-
-ifeq ($(TARGET),)
-  TARGET ?= $(BUILDHOST)
-endif
-
-ifeq ($(TARGET),macosx)
-  COMPILER ?= clang
-  TARGETNAME := macosx
-  ARCH ?= $(MACOSX_ARCH)
-  ifeq (,$(ARCH))
-    ARCH := x86_64
-  endif
-endif
-
-ifeq ($(TARGET),android)
-  TARGETNAME := android
-  ARCH ?= armv7a
-  COMPILER ?= gcc
-endif
-
-ifeq ($(TARGET),linux64)
-  TARGETNAME := linux
-  ARCH ?= x86_64
-  COMPILER ?= gcc
-  PKGARCH ?= amd64
-endif
-
-ifeq ($(TARGET),linux32)
-  TARGETNAME := linux
-  ARCH ?= i386
-  COMPILER ?= gcc
-  PKGARCH ?= x86
-endif
-
-ifeq ($(TARGET),win32)
-  TARGETNAME := win
-  ARCH ?= i386
-  COMPILER ?= vs2013
-  VARIANT:=-$(COMPILER)$(VARIANT)
-endif
-
-ifeq ($(TARGET),win64)
-  TARGETNAME := win
-  ARCH ?= x86_64
-  COMPILER ?= vs2015
-  VARIANT:=-$(COMPILER)$(VARIANT)
-endif
-
-ifeq ($(TARGET),iossim)
-  # 'iossim' is shorthand for TARGET=ios, ARCH=i386
-  override TARGET := ios
-  ARCH ?= x86_64
-endif
-
-ifeq ($(TARGET),ios)
-  TARGETNAME := ios
-  ARCH ?= armv7
-  COMPILER ?= clang
-  VARIANT:=-$(ARCH)$(VARIANT)
-endif
-
-# Give the client a chance to define their own configuration code and
-# platforms.
--include $(CUSTOMSCRIPTS)/tzbuild_config.mk
-
-# unknown
-ifeq ($(TARGETNAME),)
-  $(error Couldnt determine TARGETNAME from TARGET: $(TARGET))
-endif
+include $(BUILDDIR)/config_platform_info.mk
+# $(info tzbuild: config.mk: called config_platform_info.mk)
 
 ############################################################
 # CONFIG default settings
@@ -273,7 +133,7 @@ _dir_marker = $(foreach d,$(1),$(d).mkdir)
 # 1 - directory name
 define _create_mkdir_rule
 
-  $(if $(filter $(1),$(_TZ_DIRS)),$(error alrady have rule for dir: $1))
+  $(if $(filter $(1),$(_TZ_DIRS)),$(error already have rule for dir: $1))
 
   $(call _dir_marker,$(1)) :
 	@echo "[MKDIR] $1"
---
+++ config_platform_info.mk
@@ -0,0 +1,171 @@
+# Copyright (c) 2015 Turbulenz Limited.
+# Released under "Modified BSD License".  See COPYING for full text.
+
+ifeq ($(BUILDDIR),)
+  $(error BUILDDIR not set)
+endif
+
+# Inputs:
+#   TARGET
+# Outputs:
+#   BUILDHOST
+#   TARGETNAME
+#   TARGET (if not set)
+#   ARCH (if not set)
+#   COMPILER (if not set)
+#   VARIANT (on some platforms, otherwise this is set later in config.mk)
+
+############################################################
+# Determine the build host
+############################################################
+
+ifeq (,$(BUILDHOST))
+
+# Try to detect Windows, most robust methods first
+
+ifeq (Windows_NT,$(OS))
+  UNAME := win32
+else
+  ifneq (,$(filter %.exe,$(notdir $(SHELL))))
+    UNAME := win32
+  else
+    ifdef SYSTEMROOT
+      UNAME := win32
+    else
+      ifdef SystemRoot
+        UNAME := win32
+      else
+        ifdef COMSPEC
+          UNAME := win32
+        else
+          ifdef ComSpec
+            UNAME := win32
+          endif
+        endif
+      endif
+    endif
+  endif
+endif
+
+ifeq (,$(UNAME))
+  UNAME := $(shell uname)
+endif
+
+# Check for unsupported build host
+ifeq ($(UNAME),)
+  $(warning Couldnt determine BUILDHOST from uname: $(UNAME), assuming win32)
+  UNAME := win32
+endif
+
+# macosx
+ifeq ($(UNAME),Darwin)
+  BUILDHOST := macosx
+endif
+
+# linux32/64
+ifeq ($(UNAME),Linux)
+  M_ARCH := $(shell uname -m)
+  ifeq ($(M_ARCH),x86_64)
+    BUILDHOST := linux64
+  else
+    ifeq ($(M_ARCH),i686)
+      BUILDHOST := linux32
+    else
+      $(error Unsupported architecture: $(M_ARCH))
+    endif
+  endif
+endif
+
+# windows
+ifeq ($(UNAME),win32)
+  override SHELL := cmd.exe
+  BUILDHOST := win64
+  ABSPATHS := 0
+  UNITY := 0
+endif
+
+# $(info UNAME = $(UNAME))
+# $(info SHELL = $(SHELL))
+
+endif # ifeq (,$(BUILDHOST))
+
+# Set TARGET if it hasn't been determined, and based on that, set
+# TARGETNAME:
+#
+#  TARGET = linux32, linux64, macosx, win32, win64, android, ...
+#  TARGETNAME = linux, macosx, win, android, ...
+#  ARCH = i386,x86_64,armv7a
+#  PKGARCH = x86,amd64
+
+ifeq (,$(TARGETNAME))
+
+ifeq ($(TARGET),)
+  TARGET ?= $(BUILDHOST)
+endif
+
+ifeq ($(TARGET),macosx)
+  COMPILER ?= clang
+  TARGETNAME := macosx
+  ARCH ?= $(MACOSX_ARCH)
+  ifeq (,$(ARCH))
+    ARCH := x86_64
+  endif
+endif
+
+ifeq ($(TARGET),android)
+  TARGETNAME := android
+  ARCH ?= armv7a
+  COMPILER ?= gcc
+endif
+
+ifeq ($(TARGET),linux64)
+  TARGETNAME := linux
+  ARCH ?= x86_64
+  COMPILER ?= gcc
+  PKGARCH ?= amd64
+endif
+
+ifeq ($(TARGET),linux32)
+  TARGETNAME := linux
+  ARCH ?= i386
+  COMPILER ?= gcc
+  PKGARCH ?= x86
+endif
+
+ifeq ($(TARGET),win32)
+  TARGETNAME := win
+  ARCH ?= i386
+  COMPILER ?= vs2013
+  VARIANT:=-$(COMPILER)$(VARIANT)
+endif
+
+ifeq ($(TARGET),win64)
+  TARGETNAME := win
+  ARCH ?= x86_64
+  COMPILER ?= vs2015
+  VARIANT:=-$(COMPILER)$(VARIANT)
+endif
+
+ifeq ($(TARGET),iossim)
+  # 'iossim' is shorthand for TARGET=ios, ARCH=i386
+  override TARGET := ios
+  ARCH ?= x86_64
+endif
+
+ifeq ($(TARGET),ios)
+  TARGETNAME := ios
+  ARCH ?= armv7
+  COMPILER ?= clang
+  VARIANT:=-$(ARCH)$(VARIANT)
+endif
+
+# Give the client a chance to define their own configuration code and
+# platforms.
+-include $(CUSTOMSCRIPTS)/tzbuild_config.mk
+
+# unknown
+ifeq ($(TARGETNAME),)
+  $(error Couldnt determine TARGETNAME from TARGET: $(TARGET))
+endif
+
+endif # ifeq (,$(TARGETNAME))
