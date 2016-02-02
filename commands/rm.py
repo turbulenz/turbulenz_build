94acce4b6ae50ade1713d6a0242c6829bba8f460
Duncan Tebbs <duncan.tebbs@gmail.com>
2016-02-02 09:40:16 +0900

    Catch exceptions in rm.py (seen on Windows sometimes)

--- commands/rm.py
+++ commands/rm.py
@@ -3,13 +3,26 @@ import sys
 import os
 import shutil
 
+def try_catch_n(fn, times):
+    if 0 >= times:
+        return
+    try:
+        fn()
+    except Exception,_:
+        try_catch_n(fn, times-1)
+
 args = sys.argv
 args.pop(0)
+
 while len(args):
     d = args.pop(0)
     #print d
-    if os.path.exists(d):
-        if os.path.isdir(d):
-            shutil.rmtree(d)
-        else:
-            os.unlink(d)
+
+    def delete():
+        if os.path.exists(d):
+            if os.path.isdir(d):
+                shutil.rmtree(d)
+            else:
+                os.unlink(d)
+
+    try_catch_n(delete, 4)
