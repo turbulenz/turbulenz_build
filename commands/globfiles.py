83c6813c7aceab00ee1f305c94022e5911776ac9
Duncan Tebbs <duncan.tebbs@gmail.com>
2016-07-05 13:17:58 +0900

    globfiles.py command

---
+++ commands/globfiles.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os.path
+import sys
+import glob
+
+def main():
+
+    join = os.path.join
+
+    _wildcards = []
+    _paths = []
+
+    args = sys.argv
+    args.pop(0)
+    while 0 != len(args):
+        a = args.pop(0)
+        if "--wildcard" == a:
+            _wildcards.append(args.pop(0))
+        else:
+            _paths.append(a)
+
+    # print "_wildcard: %s" % _wildcard
+    # print "_paths: %s" % _paths
+
+    files = []
+    if 0 != len(_wildcards):
+        for p in _paths:
+            for w in _wildcards:
+                files += [f.replace('\\', '/').replace(' ', '\\ ') \
+                          for f in glob.glob(join(p, w))]
+    else:
+        for p in _paths:
+            files += [f.replace('\\', '/').replace(' ', '\\ ') \
+                      for f in glob.glob(p)]
+
+    #print sys.argv[1]
+    #sys.stderr.write("GLOB: %s,  RESULT: %s\n" % (sys.argv[1], result))
+    result = " ".join(files)
+    print "%s" % result
+    return 0
+
+if "__main__" == __name__:
+    exit(main())
